b7a3173bbfcfc134e9e3b42450dbfb21b63b4f4e
ashutosh8454 <ashutoshkumar@mitacsc.edu.in>
2021-09-14 23:03:45 +0530

    completed Fully Backend

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require("path");
+const port = 80;
+
+const app = express();
+app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+app.use(express.urlencoded());
+
+
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'img')));
+
+app.get('/', (req, res, next) => {
+
+    res.sendFile(path.join(__dirname, "./my website.html", ))
+
+})
+app.get('*', (req, res) => {
+    res.status(404).send("404")
+})
+
+app.listen(port, (req, res) => {
+
+    console.log(`servering at ${port}`);
+})
